1230153ddfec91cceafa29535acbe54a8e7696ae
Francisco <chico.ffos@gmail.com>
2014-05-31 16:07:24 -0700

    Creating standard project

---
+++ Scrapper_v1/Scrapper_v1/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for Scrapper_v1 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'Scrapper_v1'
+
+SPIDER_MODULES = ['Scrapper_v1.spiders']
+NEWSPIDER_MODULE = 'Scrapper_v1.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'Scrapper_v1 (+http://www.yourdomain.com)'
